aa7cce50d84aea763182ef716e0879b22e0ce19e
Basant Suthar <basant.suthar@prakashinfotech.com>
2021-04-12 18:53:50 +0530

    initial commit

---
+++ Blazor-Server-App/Data/CustomAuthenticationStateProvider.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Components.Authorization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Blazored.SessionStorage;
+namespace Blazor_Server_App.Data
+{
+    public class CustomAuthenticationStateProvider : AuthenticationStateProvider
+    {
+        ISessionStorageService _sessionStorageService;
+        public CustomAuthenticationStateProvider(ISessionStorageService sessionStorageService)
+        {
+            _sessionStorageService = sessionStorageService;
+        }
+        public override async Task<AuthenticationState> GetAuthenticationStateAsync()
+        {            
+            var identity = new ClaimsIdentity();
+            string email = await _sessionStorageService.GetItemAsync<string>("username");
+
+            if (!string.IsNullOrEmpty(email))
+            {
+                identity = new ClaimsIdentity(new[]
+                {
+                    new Claim(ClaimTypes.Name, email)
+                }, "apiauth_type");
+            }
+
+            var user = new ClaimsPrincipal(identity);
+
+            return await Task.FromResult(new AuthenticationState(user));
+        }
+
+        public void MarkAsAuthorize(string email)
+        {
+            var identity = new ClaimsIdentity(new[]
+            {
+                new Claim(ClaimTypes.Name, email)
+            }, "apiauth_type");
+
+            var user = new ClaimsPrincipal(identity);
+
+            NotifyAuthenticationStateChanged(Task.FromResult(new AuthenticationState(user)));
+        }
+        public void MarkAsUnAuthorize()
+        {
+            var identity = new ClaimsIdentity();
+
+            var user = new ClaimsPrincipal(identity);
+
+            NotifyAuthenticationStateChanged(Task.FromResult(new AuthenticationState(user)));
+        }
+
+    }
+}
---
+++ Blazor-Server-App/Data/UserModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Blazor_Server_App.Data
+{
+    public class UserModel
+    {
+        [Required]
+        public string Username { get; set; }
+        [Required]
+        public string Password { get; set; }
+    }
+}
